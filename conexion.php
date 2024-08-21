2ceec6d9af92a30af7ba8f7fce107b1229e1b7ff
JOSHLET4 <ecualetran@gmail.com>
2024-08-20 21:22:22 -0600

    Sistema

--- conexion.php
+++ conexion.php
@@ -1,11 +1,12 @@
 <?php 
-    $servidor = "127.0.0.1:3310";
+    $servidor = "127.0.0.1";
+    $port = "3310";
     $nombreBD = "notaseducativas";
     $usuario = "root";
     $contrasenia = "";
 
     try {
-        $conexion = new PDO("mysql:host=$servidor; dbname=$nombreBD", $usuario, $contrasenia);
+        $conexion = new PDO("mysql:host=$servidor;port=$port dbname=$nombreBD", $usuario, $contrasenia);
         $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     } catch (PDOExeption $e) {

efacf32f3576b1c7e94d022aa53209d29d86870b
Josue Letrán <ecualetran@gmail.com>
2024-08-21 14:20:13 -0600

    Sistema

--- includes/header.php
+++ includes/header.php
@@ -10,7 +10,7 @@
         <link rel="stylesheet" href="/css/styleNavBar.css" />
 </head>
 <body>
-    <nav class="navbar navbar-dark bg-dark">
+    <nav class="navbar navbar-dark bg-dark fixed-top">
       <div class="container-fluid">
         <a class="navbar-brand" href="../dashboard.php">Bienvenido, <?php echo htmlspecialchars($_SESSION['username']); ?></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">

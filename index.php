c00c7b361f417f62644f2e80df8feefa28055e78
JOSHLET4 <ecualetran@gmail.com>
2024-08-21 19:20:16 -0600

    Actualizacion Sistema

--- index.php
+++ index.php
@@ -37,9 +37,31 @@
         value="INICIAR SESION"
         class="login-submit"
       />
-    </form>
-    <a href="#" class="login-forgot-pass">¿Olvidaste tu contraseña?</a>
-    <div class="underlay-photo"></div>
-    <div class="underlay-black"></div>
-  </body>
+      <a href="#" class="login-forgot-pass" data-toggle="modal" data-target="#forgotPasswordModal">¿Olvidaste tu contraseña?</a>
+<div class="modal fade" id="forgotPasswordModal" tabindex="-1" role="dialog" aria-labelledby="forgotPasswordModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="forgotPasswordModalLabel">Olvidaste tu Contraseña</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Para cambiar su contraseña, debe contactar a soporte.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="underlay-photo"></div>
+<div class="underlay-black"></div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
 </html>

0cd8e30c9618e41446caa19a69bd61b130b28651
JOSHLET4 <ecualetran@gmail.com>
2024-08-21 19:35:05 -0600

    Actualizacion Sistema

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login</title>
-    <link rel="stylesheet" href="css/index.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/index.css" />
   </head>
   <body>
     <form action="/login/autenticacion.php" method="post" class="login-form">
